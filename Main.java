39cafe6da0ce69316d0710a5b1c98fbb5933ff27
Keila Eneas <keilaeneas@gmail.com>
2019-01-11 14:17:33 -0200

    1008 corrigido

--- Main.java
+++ Main.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
-public class Main {
-    public static void main(String[] args){
-
-      int NUMBER,hours;
-      float amount,SALARY;
-      Scanner sc = new Scanner(System.in);
-      
-      NUMBER = sc.nextInt();
-      hours = sc.nextInt();
-      amount = sc.nextFloat();
-      SALARY = hours * amount;
-
-      System.out.printf("NUMBER = %d\n",NUMBER);
-      System.out.printf("SALARY = U$ %0.2f\n", SALARY);//SALARY = U$ 4100.00 
- 
-  }
+class Main{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int num, h;
+		double sal;
+		num = scan.nextInt();
+		h = scan.nextInt();
+		sal = scan.nextDouble();
+		sal *= h;
+		System.out.println("NUMBER = " + num);
+		System.out.printf("SALARY = U$ %.2f\n", sal);
+	}
 }
